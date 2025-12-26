c2f25fb6f376ebc7ab98efb742eb5956b2ffeb3b
Bran666 <brandonstif229@gmail.com>
2025-12-26 16:54:25 -0500

    s corregida

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -17,7 +17,7 @@ venv/bin/python -m pip install -r requirements.txt
 mkdir -p reports
 
 echo "Ejecutando pruebas"
-venv/bin/python -m pytest test/ \
+venv/bin/python -m pytest tests/ \
   --junitxml=reports/test-results.xml \
   --html=reports/test-results.html \
   --self-contained-html
