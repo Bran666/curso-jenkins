e6e3e8d6345312d7d6cd2a700d0173f8e7c9aea3
Bran666 <brandonstif229@gmail.com>
2025-12-26 16:19:00 -0500

    fix: correction of VM,

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Activando el entrono virtual"
+if [!"-d venv"]; then
+    python3 -m venv venv
+fi
+
 source ven/bin/activate
 
 echo "Instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
