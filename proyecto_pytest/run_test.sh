46217d5ed1e269ad3f897e1b0dcd2e33f62255ef
Bran666 <brandonstif229@gmail.com>
2025-12-26 16:49:41 -0500

    vamo'  a probar

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -1,25 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Activando el entorno virtual"
+set -e
+
+echo "Preparando entorno"
 
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
 
-if [ -f "venv/bin/activate" ]; then
-    source venv/bin/activate
-elif [ -f "venv/Scripts/activate" ]; then
-    source venv/Scripts/activate
-else
-    echo "No se pudo activar el VM"
-    exit 1
-fi
-
 echo "Instalando dependencias"
-pip install --upgrade pip --break-system-package
-pip install -r requirements.txt --break-system-package
+venv/bin/pip install --upgrade pip
+venv/bin/pip install -r requirements.txt
+
+mkdir -p reports
 
 echo "Ejecutando pruebas"
-venv/bin/python -m tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+venv/bin/python -m pytest test/ \
+  --junitxml=reports/test-results.xml \
+  --html=reports/test-results.html \
+  --self-contained-html
 
-echo "Pruebas finalizadas"
+echo "Pruebas finalizadas correctamente"
